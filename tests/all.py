ff50fe5634c0047a9de95535dc8b0634981fbbad
Henning Schindler <henning.schindler@gmail.com>
2020-03-09 22:06:51 +0100

    isEmpty

--- tests/all.py
+++ tests/all.py
@@ -8,6 +8,9 @@ class AllTests(unittest.TestCase):
         c = Threadable(['foo', 'bar'])
         self.assertEqual('foo', c.first())
 
+    def test_empty_collection_is_empty(self):
+        c = Threadable()
+        self.assertTrue(c.isEmpty())
 
 if __name__ == '__main__':
     unittest.main()
--- threadable/threadable.py
+++ threadable/threadable.py
@@ -1,6 +1,9 @@
 class Threadable:
-    def __init__(self, values):
+    def __init__(self, values = []):
         self._values = values
 
     def first(self):
         return self._values[0]
+
+    def isEmpty(self):
+        return not self._values
