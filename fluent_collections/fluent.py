183bacb948a0afadcc28560c3d5f50b6112bdc63
Henning Schindler <henning.schindler@peerox.de>
2020-03-27 22:17:12 +0100

    replace unittest with pytest, parametrize tests

--- fluent_collections/fluent.py
+++ fluent_collections/fluent.py
@@ -1,4 +1,9 @@
+from typing import Union, Tuple, List
+
+
 class Fluent:
+    _values: Union[List, Tuple]
+
     def __init__(self, values=[]):
         self._values = values
 
--- tests/all.py
+++ tests/all.py
@@ -1,17 +1,19 @@
-import unittest
+import pytest
 
 from fluent_collections import Fluent
 
 
-class AllTests(unittest.TestCase):
-    def test_first_returns_first_item(self):
-        c = Fluent(['foo', 'bar'])
-        self.assertEqual('foo', c.first())
+@pytest.mark.parametrize("collection", [['foo', 'bar'], ('foo', 'bar')])
+def test_first_returns_first_item(collection):
+    c = Fluent(collection)
+    assert 'foo' == c.first()
 
     def test_empty_collection_is_empty(self):
         c = Fluent()
         self.assertTrue(c.is_empty())
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize("collection", [[], ()])
+def test_empty_collection_is_empty(collection):
+    c = Fluent(collection)
+    assert c.is_empty()
